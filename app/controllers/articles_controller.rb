1d07d63eaaa2a439dc894945e7fedc36861261a7
Mike Gaudreau <gaudreaum01@gmail.com>
2019-11-08 09:57:45 -0500

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,5 +19,27 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+
+    @article = Article.find(params[:id])
+    if !params[:article][:title].empty?
+      @article.update(title: params[:article][:title], description: params[:article][:description])
+    end
+
+    if !params[:article][:description].empty?
+        @article.update(description: params[:article][:description])
+    end
+
+    if @article.errors.any?
+      redirect_to articles_path
+    else
+      redirect_to article_path(@article)
+    end
+  end
+
   # add edit and update methods here
 end
